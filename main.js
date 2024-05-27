ec73d9bbcfb702bbdb5f08a635b305481f97798c
isacord <111879423+isacord@users.noreply.github.com>
2024-05-27 11:01:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,10 @@ function diminuiTamanho{
     }
 numeroSenha.textContent = tamanhoSenha;
 }
+
+function aumentaTamanho{
+    if(tamanhoSenha <20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
