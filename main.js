9c6ff494f249b50db43986ef3e40a10be547a44c
isacord <111879423+isacord@users.noreply.github.com>
2024-06-10 09:46:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -33,3 +33,43 @@ const letrasMauísculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMínusculas = 'abcdefghijklmnopqrstuvwxyz';
 const números = '0123456789';
 const símbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha()
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+}
+
+  numeroSenha.textContent = tamanhoSenha;
+  geraSenha();
+
+}  
+
+function classificaSenha(){
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');
+   } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
+    forcaSenha.classList.add('media');
+   } else if (tamanhoSenha<= 5) {
+    forcaSenha.classList.add('fraca');
+   }
+
+}
